ff54677fb8e2fcac2e3f09c36421833202f4ef91
Ivan.Burilichev <Ivan.Burilichev@leroymerlin.ru>
2022-11-01 23:33:58 +0300

    beautiful css for tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from 'react'
+import React, { ReactNode, useState, useEffect } from 'react'
 import './tabs.css'
 
 interface TabsProps {
@@ -13,10 +13,24 @@ enum TabNames {
 const Tabs = (props: TabsProps): JSX.Element => {
   const { children } = props
   const [tab, setTab] = useState(TabNames.List)
-  return <div>tabs
-        <div className='tabs'>
-            <div onClick={() => setTab(TabNames.List)}>Контакты</div>
-            <div onClick={() => setTab(TabNames.Groups)}>Группы</div>
+  const [mousePos, setMousePos] = useState<{ x: number, y: number }>({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMouseMove = (event: { clientX: any, clientY: any }): void => setMousePos({ x: event.clientX, y: event.clientY })
+    window.addEventListener('mousemove', handleMouseMove)
+    return () => {
+      window.removeEventListener(
+        'mousemove',
+        handleMouseMove
+      )
+    }
+  }, [])
+
+  const variableForGradient = (mousePos.x / window.screen.width) * 100
+  return <div>
+        <div className='tabs' style={{ background: `linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) ${variableForGradient}%, rgba(0,212,255,1) 100%)` }}>
+            <div onClick={() => setTab(TabNames.List)}>Contacts</div>
+            <div onClick={() => setTab(TabNames.Groups)}>Groups</div>
         </div>
         <div>{children[tab]}</div>
     </div>
